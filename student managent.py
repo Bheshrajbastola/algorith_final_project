f3d17cae0df715f5567c10867a55c84941b177fa
Bheshrajbastola <swikarbaastola@gmail.com>
2021-09-17 22:24:04 +0545

    programminf project

---
+++ student managent.py
@@ -0,0 +1,61 @@
+from tkinter import *
+from tkinter import ttk
+from db import Database
+
+db = Database("Student.db")
+root = Tk()
+root.title("Student  Management System")
+root.iconbitmap('C:/Users/swika/Desktop/tution.ico')
+root.geometry("1920x1080+0+0")
+root.config(bg="#2c3e50")
+root.state("zoomed")
+
+name = StringVar()
+age = StringVar()
+dob = StringVar()
+gender = StringVar()
+email = StringVar()
+contact = StringVar()
+
+# Entries Frame
+entries_frame = Frame(root, bg="green")
+entries_frame.pack(side=TOP, fill=X)
+title = Label(entries_frame, text="Student Management System", font=("Calibri", 18, "bold"), bg="green", fg="white")
+title.grid(row=0, columnspan=2, padx=10, pady=20, sticky="w")
+
+lblName = Label(entries_frame, text="Name", font=("Calibri", 16), bg="green", fg="white")
+lblName.grid(row=1, column=0, padx=10, pady=10, sticky="w")
+txtName = Entry(entries_frame, textvariable=name, font=("Calibri", 16), width=30)
+txtName.grid(row=1, column=1, padx=10, pady=10, sticky="w")
+
+lblAge = Label(entries_frame, text="Age", font=("Calibri", 16), bg="green", fg="white")
+lblAge.grid(row=1, column=2, padx=10, pady=10, sticky="w")
+txtAge = Entry(entries_frame, textvariable=age, font=("Calibri", 16), width=30)
+txtAge.grid(row=1, column=3, padx=10, pady=10, sticky="w")
+
+lbldob = Label(entries_frame, text="D.O.B", font=("Calibri", 16), bg="green", fg="white")
+lbldob.grid(row=2, column=0, padx=10, pady=10, sticky="w")
+txtDob = Entry(entries_frame, textvariable=dob, font=("Calibri", 16), width=30)
+txtDob.grid(row=2, column=1, padx=10, pady=10, sticky="w")
+
+lblEmail = Label(entries_frame, text="Email", font=("Calibri", 16), bg="green", fg="white")
+lblEmail.grid(row=2, column=2, padx=10, pady=10, sticky="w")
+txtEmail = Entry(entries_frame, textvariable=email, font=("Calibri", 16), width=30)
+txtEmail.grid(row=2, column=3, padx=10, pady=10, sticky="w")
+
+lblGender = Label(entries_frame, text="Gender", font=("Calibri", 16), bg="green", fg="white")
+lblGender.grid(row=3, column=0, padx=10, pady=10, sticky="w")
+comboGender = ttk.Combobox(entries_frame, font=("Calibri", 16), width=28, textvariable=gender, state="readonly")
+comboGender['values'] = ("Male", "Female")
+comboGender.grid(row=3, column=1, padx=10, sticky="w")
+
+lblContact = Label(entries_frame, text="Contact No", font=("Calibri", 16), bg="green", fg="white")
+lblContact.grid(row=3, column=2, padx=10, pady=10, sticky="w")
+txtContact = Entry(entries_frame, textvariable=contact, font=("Calibri", 16), width=30)
+txtContact.grid(row=3, column=3, padx=10, sticky="w")
+
+lblAddress = Label(entries_frame, text="Address", font=("Calibri", 16), bg="green", fg="white")
+lblAddress.grid(row=4, column=0, padx=10, pady=10, sticky="w")
+
+txtAddress = Text(entries_frame, width=85, height=5, font=("Calibri", 16))
+txtAddress.grid(row=5, column=0, columnspan=4, padx=10, sticky="w")
